37df4c67d49fa144d1792648fe10b34794011f93
yosuke.aramaki <gnx.vw903@gmail.com>
2019-12-21 09:41:42 +0900

    set development in truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -2,11 +2,11 @@ module.exports = {
 
   networks: {
 
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
 
     // ropsten: {
       // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
