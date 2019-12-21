ae0bb37a391e52ce5d9d6a7ae6928315e837a129
yosuke.aramaki <gnx.vw903@gmail.com>
2019-12-21 19:35:50 +0900

    add web3.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import web3 from './web3.js'
+import GameContract from './abis/GameContract.json'
 
-function App() {
-  return (
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {   
+    try {
+      const networkId = await web3.eth.net.getId()
+      const gameContract = new web3.eth.Contract(GameContract.abi, GameContract.networks[networkId].address)
+      this.setState({ gameContract })  
+      const accounts = await web3.eth.getAccounts()
+      this.setState({ account: accounts[0] })
+    } catch (error) {
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  }
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
-  );
-}
+    )
+  }
+}  
 
 export default App;
